8e44588254c372b9c3b09138e5bd64397feb0c46
Cianna <missciannadawn@gmail.com>
2022-05-22 13:42:28 -0700

    Added a footer and header file to the php folder.

---
+++ php/header.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <!-- Css  -->
+    <link rel="stylesheet" type="text/css" href="index/style.css">
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/c70abeedb1.js" crossorigin="anonymous"></script>
+        
+
+    <title>Astre Learning</title>
+  </head>
+  <body>
+
+<!--------------------------------------------------------------------------------
+ *  - HEADER - Main Nav
+----------------------------------------------------------------------------------->
+<header>
+ <div class="zone teal">
+    
+        <ul class="main-nav">
+            
+            <li><a href="" class="logo"><i class="fa-solid fa-lightbulb"></i></i>Astre Learning</a></li>
+            <!-- Add <span class="account_name"></span> to have Account name appear -->
+            <div class="push">
+            <li><a href=""></i>Account</a></li>
+            <span onclick="openNav()">
+            <li>
+                <button type="button" class="btn shopping-cart-btn">
+                    <i class="fa fa-shopping-cart" style="font-size:20px"></i>
+                </button>
+            </li>
+        </span>
+            </div>
+        </ul>
+ </div>
+</div>
+
+<!----------------------------------------------------------------------------------- 
+    HEADER - toggle-navbar-2  
+----------------------------------------------------------------------------------->
+
+ <nav class="navbar navbar-2 navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <div class="row">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Shop All <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link purple" style="color: white" href="#">Astronomy</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link green" style="color: white" href="#">Biology</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link yellow" style="color: white" href="#">Chemistry</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link blue" style="color: white" href="#">Math</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link blue" style="color: white" href="#">Physics</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link red" style="color: white" href="#">Tecnology</a>
+          </li>
+      </ul>
+      </div>
+    </div>
+</div>
+  </nav>
+</header>
