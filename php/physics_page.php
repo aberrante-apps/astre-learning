b7c2f290121a0e504315120658be2ebd2ba5a3ff
Cianna <missciannadawn@gmail.com>
2022-05-24 22:35:16 -0700

    Added pages for each category, and I linked pages to <ahrefs> in the header

---
+++ php/physics_page.php
@@ -0,0 +1,76 @@
+<?php
+// This is a webpage for Physics Products 
+include ('header.php');
+include ('connection.php');
+
+?>
+
+<!-------------------------------------------------------------------
+ *  COVER - for Physics
+ --------------------------------------------------------------------->
+ <div class="container zone orange cover">Physics</div>
+
+
+ <!-------------------------------------------------------------------
+ *  PRODUCTS - for Physics
+ --------------------------------------------------------------------->
+   <?php
+$sql = 'SELECT Products.id, Products.name, Products.price, ProductCategories.category_id
+        FROM Products
+        LEFT JOIN ProductCategories ON Products.id=ProductCategories.product_id
+        WHERE ProductCategories.category_id = 5;';
+$result = mysqli_query($dbc, $sql);
+?>
+<!-- HTML container for catalogue -->
+<div class="container">
+            <section id="catalogue" class="section">
+                <!--Insert cards here-->
+                        <div class="row" id="cards">
+<?php
+// 
+while ($row = $result -> fetch_assoc()) 
+{
+
+// Render Product Cards
+echo "<div class='col-sm-6 col-md-4 col-lg-3 d-flex align-items-stretch' id='card-container'>";
+// id
+echo "<div class='card text-center' id='" . $row['id'] . "' style='width: 18rem;'>";
+// image
+echo "<img class ='img card-img-top' src='https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png' alt='...'>";
+// button
+echo "<button class='btn add-to-cart' data-id='" . $row['id'] . "' onclick='AddToCart(" . $row['id'] .  ")'>Add To Cart</button>";
+echo "<div class='card-body'>";
+// name
+echo "<h5 class='card-title' id='card-title'>" . $row['name'] . "</h5>";
+# echo "<p class='card-text'>" . $row['description'] . "</p>";
+// price
+echo "<p class='card-price'>" . $row['price'] . "</p>";
+echo        "</div>";
+echo    "</div>";
+echo "</div>";
+
+} // END of while loop
+  
+?>
+<!-- closing tags for HTML container and Catalogue -->
+</div>
+              </div>
+            </section>
+          </div>
+
+<!-- END --------------------------------------------------------------------------------- -->
+
+<?php
+include ('footer.php');
+?>
+
+
+
+  
+  
+
+</body>
+</html>
+<?php
+// include ('footer.php');
+?>
