1bc28c1e277e4691c077920b181e8d1874e9fc9f
Cianna <missciannadawn@gmail.com>
2022-06-01 18:10:23 -0700

    Homepage and category pages are now connected to the shopping cart. Minor style edits to the footer and header were also made.

--- php/homepage.php
+++ php/homepage.php
@@ -18,43 +18,43 @@ include ('header.php');
  *  Catalogue Preview 
  ------------------------------------------------------------------------->
 
-<?php
-$sql = 'SELECT * FROM Products;';
-$result = mysqli_query($dbc, $sql);
-?>
-
 <!-- HTML container for catalogue -->
 <div class="container">
-            <section id="catalogue" class="section">
-                <!--Insert cards here-->
-                        <div class="row" id="cards">
+    <section id="catalogue" class="section">
+        <!--Insert cards here-->
+            <div class="row" id="cards">
 <?php
-while ($row = $result -> fetch_assoc()) 
+$sql = 'SELECT * FROM Products ORDER BY id ASC';
+$result = mysqli_query($dbc, $sql);
+
+if(mysqli_num_rows($result) > 0) 
 {
-echo "<div class='col-sm-6 col-md-4 col-lg-3 d-flex align-items-stretch' id='card-container'>";
-echo    "<div class='card text-center' id='" . $row['id'] . "' style='width: 18rem;'>";
-echo        "<img class ='img card-img-top' src='" . $row['picture'] . "' alt='https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png'>";
-echo         "<div class='card-body'>";
-echo            "<h5 class='card-price'>" . $row['price'] . "</h5>";
-echo            "<h6 class='card-title' id='card-title'>" . $row['name'] . "</h6>";
-// echo            "<p class='card-text'>" . $row['description'] . "</p>";
+    while ($row = mysqli_fetch_assoc($result))
+    {
+        ?>
+        <div class="col-sm-6 col-md-4 col-lg-3 d-flex align-items-stretch"  id="card-container">
+        <div class="card text-center" style="width: 18rem;">
+        <form method="POST" action="display-products.php?action=add&id=<?php echo $row['id']; ?>">
+        <img class ="img card-img-top" src=" <?php echo $row['picture']; ?>" alt="https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png">
+        <div class="card-body">
+        <h5 class="card-price">$<?php echo $row['price']; ?></h5>
+        <h6 class="card-title" id="card-title"><?php echo $row['name']; ?></h6>
+        <input type="hidden" name="quantity" value="1" class="form-control">
+        <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" >
+        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" >
+        <button type="submit" name="add_to_cart" class="btn add-to-cart text-center" value="Add to Cart">Add to Cart</button>
 
-echo        "</div>";
-echo                "<button class='btn add-to-cart' data-id='" . $row['id'] . 
-                    "' onclick='AddToCart(" . $row['id'] .  ")'>Add To Cart</button>";
-echo    "</div>";
-echo "</div>";
+        </div>
+        </form>
+        </div>
+        </div>
+<?php
+    }
 }
-    
 ?>
-</div>
-              </div>
-            </section>
-          </div>
-
-</body>
-</html>
-
+        </div>
+        </section>
+        </div>
 <?php
 include ('footer.php');
 ?>
--- php/page_technology.php
+++ php/page_technology.php
@@ -28,31 +28,34 @@ $result = mysqli_query($dbc, $sql);
                         <div class="row" id="cards">
 <?php
 // 
-while ($row = $result -> fetch_assoc()) 
+if(mysqli_num_rows($result) > 0) 
 {
+    while ($row = mysqli_fetch_assoc($result))
+    {
+        ?>
+        <div class="col-sm-6 col-md-4 col-lg-3 d-flex align-items-stretch"  id="card-container">
+        <div class="card text-center" style="width: 18rem;">
+        <form method="POST" action="display-products.php?action=add&id=<?php echo $row['id']; ?>">
+        <img class ="img card-img-top" src=" <?php echo $row['picture']; ?>" alt="https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png">
+        <div class="card-body">
+        <h5 class="card-price">$<?php echo $row['price']; ?></h5>
+        <h6 class="card-title" id="card-title"><?php echo $row['name']; ?></h6>
+        <input type="hidden" name="quantity" value="1" class="form-control">
+        <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" >
+        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" >
+        <button type="submit" name="add_to_cart" class="btn add-to-cart text-center" value="Add to Cart">Add to Cart</button>
 
-// Render Product Cards
-echo "<div class='col-sm-6 col-md-4 col-lg-3 d-flex align-items-stretch' id='card-container'>";
-echo    "<div class='card text-center' id='" . $row['id'] . "' style='width: 18rem;'>";
-echo        "<img class ='img card-img-top' src='" . $row['picture'] . "' alt='https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png'>";
-echo         "<div class='card-body'>";
-echo            "<h5 class='card-title' id='card-title'>" . $row['name'] . "</h5>";
-// echo            "<p class='card-text'>" . $row['description'] . "</p>";
-echo            "<p class='card-price'>" . $row['price'] . "</p>";
-echo        "</div>";
-echo                "<button class='btn add-to-cart' data-id='" . $row['id'] . 
-                    "' onclick='AddToCart(" . $row['id'] .  ")'>Add To Cart</button> </p>";
-echo    "</div>";
-echo "</div>";
-} // END of while loop
-  
+        </div>
+        </form>
+        </div>
+        </div>
+<?php
+    }
+}
 ?>
-<!-- closing tags for HTML container and Catalogue -->
-</div>
-              </div>
-            </section>
-          </div>
-
+        </div>
+        </section>
+        </div>
 <!-- END --------------------------------------------------------------------------------- -->
 
 <?php
