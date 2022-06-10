9e884a4687a19687150bda5f35ad055a59abf106
Matthew Zerrath <matthewzerrath@hotmail.com>
2022-06-10 16:46:02 -0700

    Brought back most of Cianna's checkout modal. Will figure out how to integrate all the customer information later.

--- php/shopping-cart/charge.php
+++ php/shopping-cart/charge.php
@@ -1,8 +1,24 @@
 <?php
   require_once('./config.php');
 
+  // Basic POST variables
   $token  = $_POST['stripeToken'];
   $email  = $_POST['stripeEmail'];
+
+  // POST variables for customer billing address
+  $billingAddress = $_POST[''];
+  $billingCity = $_POST[''];
+  $billingProvince = $_POST[''];
+  $billingPostalCode = $_POST[''];
+
+  // POST variables for shipping address
+  $shippingName = $_POST[''];
+  $shippingAddress = $_POST[''];
+  $shippingCity = $_POST[''];
+  $shippingProvince = $_POST[''];
+  $shippingPostalCode = $_POST[''];
+
+  // POST variables for the price of the order
   $grandTotal = $_POST['stripeTotal'];
   $grandTotalPrice = number_format(($grandTotal/100),2);
 
@@ -16,6 +32,4 @@
       'amount'   => "$grandTotal",
       'currency' => 'cad',
   ]);
-
-  echo "<h1>Successfully charged $$grandTotalPrice!</h1>";
 ?>
