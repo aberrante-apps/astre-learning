5f41b628d622098486c581f2136daeb95d2b16a3
Cianna <missciannadawn@gmail.com>
2022-06-18 11:54:53 -0700

    minor styling edits done to product cards. Font-size, bolded price and add2cart, moved button below img, added a new color on hover.

--- php/homepage.php
+++ php/homepage.php
@@ -34,8 +34,9 @@ if(mysqli_num_rows($result) > 0)
         ?>
         <div class="col-sm-6 col-md-4 col-lg-3 d-flex align-items-stretch"  id="card-container">
         <div class="card text-center" style="width: 18rem;">
-        <form method="POST" action="homepage.php?action=add&id=<?php echo $row['id']; ?>">
+        <form method="POST" style="height:100%;" action="homepage.php?action=add&id=<?php echo $row['id']; ?>">
         <img class ="img card-img-top"  src="<?php echo $row['picture']; ?>" alt="https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png">
+        <button type="submit" name="add_to_cart" class="btn add-to-cart text-center" value="Add to Cart">Add to Cart</button>
         <div class="card-body">
         <h5 class="card-price">$<?php echo $row['price']; ?></h5>
         <h6 class="card-title" id="card-title"><?php echo $row['name']; ?></h6>
@@ -44,9 +45,10 @@ if(mysqli_num_rows($result) > 0)
         <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" >
         <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" >
         
-        <button type="submit" name="add_to_cart" class="btn add-to-cart text-center" value="Add to Cart">Add to Cart</button>
+        
 
         </div>
+        
         </form>
         
         </div>
